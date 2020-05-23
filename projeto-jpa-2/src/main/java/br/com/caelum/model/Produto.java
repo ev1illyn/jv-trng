7406f042961ee6f97b50f6f210df393cf5be51a7
evillyn <evillyn.oliveira@estagiario.ufca.edu.br>
2020-05-23 19:48:37 -0300

    setting entity manager in view interceptor

---
+++ projeto-jpa-2/src/main/java/br/com/caelum/model/Produto.java
@@ -0,0 +1,127 @@
+
+package br.com.caelum.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedAttributeNode;
+import javax.persistence.NamedEntityGraph;
+import javax.persistence.NamedEntityGraphs;
+import javax.persistence.OneToMany;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/*@NamedEntityGraphs({
+    @NamedEntityGraph(name = "produtoComCategoria", 
+                      attributeNodes = { 
+                            @NamedAttributeNode("categorias") 
+                      }) 
+})*/
+
+@Entity
+@DynamicUpdate
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@NotEmpty
+	private String nome;
+	@NotEmpty
+	private String linkDaFoto;
+	
+	@NotEmpty
+	@Column(columnDefinition="TEXT")
+	private String descricao;
+	
+	@Min(20)
+	private double preco;
+	
+	@ManyToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)
+    @Fetch(FetchMode.SUBSELECT)
+	private List<Categoria> categorias = new ArrayList();
+	
+	@Valid
+	@ManyToOne
+	private Loja loja;
+	
+	
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	//método auxiliar para associar categorias com o produto
+	//se funcionar apos ter definido o relacionamento entre produto e categoria
+	public void adicionarCategorias(Categoria... categorias) {
+		for (Categoria categoria : categorias) {
+			this.categorias.add(categoria);
+		}
+	}
+
+	public String getLinkDaFoto() {
+		return linkDaFoto;
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public void setLinkDaFoto(String linkDaFoto) {
+		this.linkDaFoto = linkDaFoto;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setLoja(Loja loja) {
+		this.loja = loja;
+	}
+
+	public Loja getLoja() {
+		return loja;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+
+}
