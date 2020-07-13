1c7721b860b31b98bcf1aabc35dae4d3f3666402
ev1illyn <evillyndsoliveiras@gmail.com>
2020-07-12 21:26:32 -0300

    validating form data

--- jsf-livraria/src/br/com/caelum/livraria/bean/LivroBean.java
+++ jsf-livraria/src/br/com/caelum/livraria/bean/LivroBean.java
@@ -2,8 +2,12 @@ package br.com.caelum.livraria.bean;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
@@ -45,11 +49,19 @@ public class LivroBean {
 		this.livro.adicionaAutor(autor);
 	}
 	
+	public void comecaComDigitoUm(FacesContext fc, UIComponent component, Object value) throws ValidatorException {
+		String valor = value.toString();
+		if (!valor.startsWith("1")) {
+			throw new ValidatorException(new FacesMessage("Deveria começar com 1"));
+		}
+	}
+	
 	public void gravar() {
-		System.out.println("Gravando livro " + this.livro.getTitulo());
 
 		if (livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			/* throw new RuntimeException("Livro deve ter pelo menos um Autor."); */
+			FacesContext.getCurrentInstance().addMessage("autor", new FacesMessage("Livro deve ter pelos menos um autor"));
+			return;
 		}
 
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
