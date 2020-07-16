038f3ad19196fadbf4c294fe47b8f1a67f080ce4
ev1illyn <evillyndsoliveiras@gmail.com>
2020-07-15 23:03:14 -0300

    setting user authorization and authentication with phaselistener

---
+++ jsf-livraria/src/br/com/caelum/livraria/bean/LoginBean.java
@@ -0,0 +1,32 @@
+package br.com.caelum.livraria.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import br.com.caelum.livraria.dao.UsuarioDAO;
+import br.com.caelum.livraria.modelo.Usuario;
+import br.com.caelum.livraria.util.RedirectView;
+
+@ManagedBean
+@ViewScoped
+public class LoginBean {
+
+	private Usuario usuario = new Usuario();
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	
+	public RedirectView logar() {
+		boolean existe = new UsuarioDAO().existe(this.usuario);
+		if (existe) {
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
+			return new RedirectView("livro?faces-redirect=true");
+		} else {
+			return new RedirectView("login?faces-redirect=true");
+		}
+	}
+	
+}
