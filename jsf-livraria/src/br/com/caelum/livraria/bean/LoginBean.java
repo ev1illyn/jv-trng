be44d06ec597ba07a4beb036a9b99933e21b5dcd
ev1illyn <evillyndsoliveiras@gmail.com>
2020-07-15 23:39:39 -0300

    setting logout and flash scope

--- jsf-livraria/src/br/com/caelum/livraria/bean/LoginBean.java
+++ jsf-livraria/src/br/com/caelum/livraria/bean/LoginBean.java
@@ -1,5 +1,6 @@
 package br.com.caelum.livraria.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
@@ -19,14 +20,22 @@ public class LoginBean {
 	}
 	
 	public RedirectView logar() {
+		FacesContext context = FacesContext.getCurrentInstance();
 		boolean existe = new UsuarioDAO().existe(this.usuario);
+		
 		if (existe) {
-			FacesContext context = FacesContext.getCurrentInstance();
 			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
 			return new RedirectView("livro?faces-redirect=true");
 		} else {
+			context.getExternalContext().getFlash().setKeepMessages(true);;
+			context.addMessage(null, new FacesMessage("Usuário não encontrado"));
 			return new RedirectView("login?faces-redirect=true");
 		}
 	}
 	
+	public RedirectView deslogar() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.getExternalContext().getSessionMap().remove("usuarioLogado");
+		return new RedirectView("login?faces-redirect=true");
+	}
 }
