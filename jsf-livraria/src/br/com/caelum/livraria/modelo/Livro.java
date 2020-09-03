05a405728ca17b505ee4b41db1d51feb228990d6
ev1illyn <evillyndsoliveiras@gmail.com>
2020-09-03 14:26:20 -0300

    setting sales graphs

--- jsf-livraria/src/br/com/caelum/livraria/modelo/Livro.java
+++ jsf-livraria/src/br/com/caelum/livraria/modelo/Livro.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -20,6 +21,7 @@ public class Livro {
 	private Integer id;
 
 	private String titulo;
+	private String genero;
 	private String isbn;
 	private double preco;
 	@Temporal(TemporalType.TIMESTAMP)
@@ -53,6 +55,14 @@ public class Livro {
 
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
 	}
 
 	public String getIsbn() {
