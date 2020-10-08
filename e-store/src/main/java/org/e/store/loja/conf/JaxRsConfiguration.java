530c1b8ed19d008a8a604ad6a99f078de01c92db
ev1illyn <evillyndsoliveiras@gmail.com>
2020-10-08 15:58:19 -0300

    config JAX-RS

---
+++ e-store/src/main/java/org/e/store/loja/conf/JaxRsConfiguration.java
@@ -0,0 +1,9 @@
+package org.e.store.loja.conf;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/services")
+public class JaxRsConfiguration extends Application{
+
+}
---
+++ e-store/src/main/java/org/e/store/loja/service/PagamentoService.java
@@ -0,0 +1,15 @@
+package org.e.store.loja.service;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+@Path("/pagamento")
+public class PagamentoService {
+
+	@POST
+	public Response pagar(@QueryParam("uuid") String uuid) {
+		return null;
+	}
+}
