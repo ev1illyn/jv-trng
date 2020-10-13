c96126f1f5afa3902b8b8bfb0df4c3f5492150fe
ev1illyn <evillyndsoliveiras@gmail.com>
2020-10-13 18:51:10 -0300

    creating password generator and system roles and user

---
+++ e-store/src/main/java/org/e/store/loja/models/SystemRole.java
@@ -0,0 +1,24 @@
+package org.e.store.loja.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class SystemRole {
+
+	@Id
+	private String name;
+
+	public SystemRole(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
